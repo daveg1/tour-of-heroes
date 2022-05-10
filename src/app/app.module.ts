15aa3b33c0ec07c23cfac7669ca11c6e043988db
daveg1 <d.graham8@rgu.ac.uk>
2022-05-10 02:04:31 +0100

    move hero-details/ to hero-detail/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
+import { AppComponent } from './app.component'
+import { HeroesComponent } from './components/heroes/heroes.component'
 
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './components/hero-details/hero-detail.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './modules/app-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HeroLinkComponent } from './components/controls/hero-link/hero-link.component';
+import { FormsModule } from '@angular/forms'
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component'
+import { MessagesComponent } from './components/messages/messages.component'
+import { AppRoutingModule } from './modules/app-routing.module'
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { HeroLinkComponent } from './components/controls/hero-link/hero-link.component'
 
 @NgModule({
   declarations: [
